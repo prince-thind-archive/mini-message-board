93b92f4d5f75e89cfafd75601dd8f7e262f9d851
Prince Thind <princethind799@gmail.com>
2021-07-05 21:32:56 +0530

    add messages and routers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form', {});
+});
+
+router.post('/new', function (req, res, next) {
+  const name = req.body.author;
+  const text = req.body.text;
+  const date = new Date();
+  messages.push({
+    text: name,
+    user: text,
+    added: date,
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
